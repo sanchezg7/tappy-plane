eb16d02b4f4703ef165b9a6dd2a121b1775a08d8
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-17 20:43:19 -0400

    Adding gravity and working on barrier WIP

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
 
+const GRAVITY: float = 800.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	move_and_slide()
