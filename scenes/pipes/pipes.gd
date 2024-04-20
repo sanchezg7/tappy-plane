8f7ccebde90bef4caae393ca11c792ad7b5c5eb5
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-19 23:50:31 -0400

    pipe scrolling across the screen now

---
+++ scenes/pipes/pipes.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+const SCROLL_SPEED: float = 160.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	position.x -= SCROLL_SPEED * delta
+	pass
