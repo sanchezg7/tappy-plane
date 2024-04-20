fbc75921d9f4c77c0409ec7fe300afc0406b5dce
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-20 11:10:33 -0400

    Subscribes for body entered and exit

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -15,3 +15,11 @@ func _physics_process(delta):
 
 func _on_screen_exited():
 	queue_free()
+
+
+func _on_pipe_body_entered(body):
+	print("On pipe body entered ", body)
+
+
+func _on_laser_body_exited(body):
+	print("On laser body exited ", body)
