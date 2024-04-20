dafd5e80ea9b57f1b573965970c295b534faaf19
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-20 13:24:22 -0400

    move main into scenes space

--- global/game.gd
+++ global/game.gd
@@ -3,7 +3,7 @@ extends Node
 const GROUP_PLAYER: String = "player"
 
 var game_scene: PackedScene = preload("res://scenes/game/game.tscn")
-var main_scene: PackedScene = preload("res://ui/main/main.tscn")
+var main_scene: PackedScene = preload("res://scenes/main/main.tscn")
 
 func load_game_scene() -> void:
 	get_tree().change_scene_to_packed(game_scene)
